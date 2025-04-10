42c6f1158c3381b6240a1b62fa01b363149dd921
Mariah <alinemariah@hotmail.com>
2025-04-10 14:32:36 +0200

    fix: remove campo password from user object before saving to database

--- src/user.ts
+++ src/user.ts
@@ -14,10 +14,13 @@ export function hashPassword(user: UserRequest) {
     const salt = bcrypt.genSaltSync(13);
     const passwordHash = bcrypt.hashSync(user.password, salt);
 
+    // Retorna apenas os campos esperados pelo Prisma
+    //Essa estrutura (const {password, ...userData} = user ) quer dizer: #Pegue o campo password de dentro de user, Guarde tudo o que não for password no novo objeto chamado userData
+    const {password, ...userData} = user 
+
     return {
-      ...user,
-      password: undefined,
-      passwordHash,
+      ...userData,
+      passwordHash, // senha criptografada
     };
   }
 
